e3d65573a14e8b8a3c8d97d7ce97bdaaf32ea33f
YazanAlhalabi <yazanalhalabi1@gmail.com>
2022-12-22 11:57:28 +0200

    generate memes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+
+import { Modal } from './Modal'
+import { Meme } from './types'
+
+import './App.css'
+
+type ModalState = Pick<Meme, 'id' | 'name' | 'blank' | 'lines'>
 
 function App() {
+  const [memes, setMemes] = useState<Meme[] | null>(null)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [currentModal, setCurrentModal] = useState<ModalState>({
+    id: '',
+    name: '',
+    blank: '',
+    lines: 1,
+  })
+
+  useEffect(() => {
+    fetch('https://api.memegen.link/templates')
+      .then((data) => data.json())
+      .then((res) => {
+        setMemes(res)
+      })
+  }, [])
+
+  const handleOpenModal = ({ id, name, blank, lines }: ModalState) => {
+    setCurrentModal({ id, name, blank, lines })
+    setIsModalOpen(true)
+  }
+  const handleCloseModal = () => {
+    setCurrentModal({ id: '', name: '', blank: '', lines: 1 })
+    setIsModalOpen(false)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 className="primary-gradient">MEME GENERATOR</h1>
+        <p>Choose a template and customize the text to your likings</p>
       </header>
+      <section className="memes">
+        {isModalOpen && currentModal.id && (
+          <Modal
+            id={currentModal.id}
+            name={currentModal.name}
+            blank={currentModal.blank}
+            lines={currentModal.lines}
+            handleCloseModal={handleCloseModal}
+          />
+        )}
+        {memes?.map((meme) => {
+          return (
+            <div key={meme.id} className="card">
+              <img src={meme.blank} alt={meme.name} loading="lazy" />
+              <p>{meme.name}</p>
+              <button
+                className="btn btn-primary"
+                type="button"
+                onClick={() =>
+                  handleOpenModal({
+                    id: meme.id,
+                    name: meme.name,
+                    blank: meme.blank,
+                    lines: meme.lines,
+                  })
+                }
+              >
+                Generate
+              </button>
+            </div>
+          )
+        })}
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
